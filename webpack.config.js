dea7ea4fb3dd0c1b62c1e0e6b8ad484710811309
wonjinsong <msmk22239@gmail.com>
2021-03-19 23:50:34 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const RefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+module.exports = {
+  name: "games",
+  mode: "development",
+  devtool: "eval",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+
+  entry: {
+    app: "./client",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  browsers: ["> 1% in CA"], //browserslist
+                },
+                debug: true,
+              },
+            ],
+            "@babel/preset-react",
+          ],
+          plugins: [
+            "@babel/plugin-proposal-class-properties",
+            "react-refresh/babel",
+          ],
+        },
+        exclude: path.join(__dirname, "node_modules"),
+      },
+    ],
+  },
+  plugins: [new RefreshWebpackPlugin()],
+
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+    publicPath: "/dist",
+  },
+  devServer: {
+    publicPath: "/dist",
+    hot: true,
+  },
+};
