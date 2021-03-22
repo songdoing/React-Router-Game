ef70b0b7aec61d0663a4c36032a1167debab06cd
wonjinsong <msmk22239@gmail.com>
2021-03-22 17:05:00 -0400

    dynamic route 2

--- gameMatcher.jsx
+++ gameMatcher.jsx
@@ -1,5 +1,8 @@
+import React, { Component } from 'react';
+
 export default class GameMatcher extends Component {
     render() {
+        console.log(this.props);
         return (
             <div>Game Matcher</div>
         );
--- games.jsx
+++ games.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Link} from 'react-router-dom';
 import NumberBaseball from '../numberBaseball/numberBaseballClass';
 import Rps from '../rockPaperScissors/rpsClass';
 import Lotto from '../lotto/lottoClass';
+import GameMatcher from './gameMatcher';
 
 const Games = () => {
     
